69d398e7c926d66f51600c70c6393b4776a9fd3f
YoshyDev <edith.canelo@yanbal.com>
2025-10-04 15:34:02 -0500

    funcion

---
+++ ElBrayan.py
@@ -0,0 +1,27 @@
+def factorial(n):
+    result = 1
+    if n>=0:
+        if n==0:
+            return 1
+        else:
+            for i in range(n):
+                result = result * (i+1)
+                i = i + 1
+            
+            return result
+    else:
+        print("Ingresa un número positivo")
+
+def factorial_recursivo(n):
+    if n>=0:
+        if n==0:
+            return 1
+        else:
+            result = n * factorial_recursivo(n-1)
+            
+            return result
+    else:
+        print("Ingresa un número positivo")
+
+
+print(factorial_recursivo(4))
